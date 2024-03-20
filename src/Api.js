83cc24d31faeb7d42ab6bf9f03bd06e0def40e5a
light-house-labs <162984600+light-house-labs@users.noreply.github.com>
2024-03-20 23:47:17 +0530

    Update Api.js

--- src/Api.js
+++ src/Api.js
@@ -4,7 +4,7 @@ const api = axios.create({
     //baseURL: 'http://192.168.137.1/api',
     //   baseURL: 'http://localhost:8081',
     //baseURL: 'https://halahomes.waysdatalabs.com/api',
-    baseURL: 'https://halahomes.ca/api',
+    baseURL: 'https://urban-homes-backend.onrender.com',
     withCredentials: true,
 });
 
